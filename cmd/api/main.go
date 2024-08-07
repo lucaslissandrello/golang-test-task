808429b9785808f0341804c7aa04e727d12bea07
Lucas Lissandrello <lucas@Air-de-Lucas.box>
2024-08-07 18:38:12 +0200

    solution

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,14 +2,52 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
+	"twitch_chat_analysis/cmd/broker"
+	"twitch_chat_analysis/cmd/consumers"
+	controller "twitch_chat_analysis/cmd/controllers"
+	"twitch_chat_analysis/cmd/redis"
+	"twitch_chat_analysis/cmd/services"
 )
 
 func main() {
 	r := gin.Default()
 
+	redisClient := redis.ConnectRedis("localhost", "", "user", "password", 0)
+	pushController := controller.PusherController{
+		RabbitMQChannel: nil, //TODO create rabbitMQChannel
+		RabbitKey:       "RabbitKey",
+	}
+
+	messageController := controller.MessagesController{MessagesService: services.MessageService{RedisClient: redisClient}}
+
+	MessageConsumer := consumers.MessageConsumer{
+		RedisClient: redisClient,
+	}
+
+	queueName := "queueName"
+
+	brokerConnection, connectErr := broker.ConnectBroker("localhost:5672")
+	if connectErr != nil {
+		log.Fatal("error connecting to broker")
+	}
+
+	consumers, consumerErr := broker.CreateConsumer(queueName, *brokerConnection)
+	if consumerErr != nil {
+		log.Fatalf("error creating consumer to queue: %s", queueName)
+	}
+
+	go func() { //TODO See how to stop this
+		for message := range consumers {
+			MessageConsumer.ProcessMessages(message)
+		}
+	}()
+
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, "worked")
 	})
 
+	r.POST("/message", pushController.PushMessageToQueue)
+	r.GET("/message/list", messageController.GetMessages)
 	r.Run()
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module twitch_chat_analysis
 go 1.18
 
 require github.com/gin-gonic/gin v1.8.1
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/streadway/amqp v1.1.0 // indirect
+)
